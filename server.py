d8db9972c8979b14e27b8c8082809fa13d565f88
Nick Anderson <i.anderson.nick@gmail.com>
2022-06-02 07:47:28 -0700

    Adding and editing pet info implimented

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from sqlite3 import connect
 from flask import Flask, render_template, redirect, request, flash, session
-from wtforms import Form, BooleanField, StringField, PasswordField, SubmitField, validators, IntegerField
+from wtforms import Form, BooleanField, StringField, DateField, PasswordField, SubmitField, validators, IntegerField
 from wtforms.validators import InputRequired, Email
 from flask_wtf import FlaskForm
 from flask_login import LoginManager, login_required, login_user, logout_user, current_user
@@ -78,11 +78,56 @@ def profile():
 
     return render_template('profile.html')
 
+@app.route('/add_pet', methods=["GET"])
+def add_pet():
+    form = PetRegistrationForm()
+    return render_template('add_pet.html', form=form)
+
+@app.route('/add_pet', methods=["POST"])
+def create_pet():
+    pet_name = request.form['name']
+    breed = request.form['breed']
+    birthday = request.form['birthday']
+    pet = Pet(pet_name = pet_name, user_id = current_user.user_id, breed = breed, birthday = birthday)
+    db.session.add(pet)
+    db.session.commit()
+    flash(f"Your pet {pet_name} has been added to your account!")
+    return render_template('homepage.html')
+
+
 @app.route('/logout')
 def logout():
     logout_user()
     flash("You have been logged out")
     return render_template('homepage.html')
+
+@app.route('/pets')
+def display_pets():
+    return render_template('pets.html')
+
+@app.route('/edit_pet/<pet_id>', methods=["GET"])
+def edit_pet(pet_id):
+    pet = Pet.query.filter_by(pet_id = pet_id).first()
+    form = PetEditForm()
+    form.name.data = pet.pet_name
+    form.breed.data = pet.breed
+    form.birthday.data = pet.birthday
+
+    return render_template('edit_pet.html', pet=pet, form=form)
+
+@app.route('/edit_pet/<pet_id>', methods=["POST"])
+def update_pet(pet_id):
+    pet = Pet.query.filter_by(pet_id = pet_id).first()
+    pet_name = request.form['name']
+    breed = request.form['breed']
+    birthday = request.form['birthday']
+    pet.pet_name = pet_name
+    pet.breed = breed
+    pet.birthday = birthday
+    db.session.commit()
+    flash(f"{pet_name}'s info has been successfully updated!")
+    return render_template('homepage.html')
+
 
 class RegistrationForm(FlaskForm):
     email = StringField('Email Address', [InputRequired('Please enter your email address.'), Email('This field requires a valid email address')])
@@ -95,6 +140,18 @@ class LoginForm(FlaskForm):
     email = StringField('Email Address', [InputRequired('Please enter your email address.'), Email('This field requires a valid email address')])
     password = PasswordField('Enter your password', [InputRequired('Please enter your password')])
     submit = SubmitField("Login")
+
+class PetRegistrationForm(FlaskForm):
+    name = StringField("Name", [InputRequired("Enter your pet's name")] )
+    breed = StringField("Breed", [InputRequired("Enter the breed of your pet")])
+    birthday = DateField("Birthday", [InputRequired("Enter your pet's birthday (or best guess)")])
+    submit = SubmitField("Add Pet")
+
+class PetEditForm(FlaskForm):
+    name = StringField("Name", [InputRequired("Update your pet's name")] )
+    breed = StringField("Breed", [InputRequired("Update the breed of your pet")])
+    birthday = DateField("Birthday", [InputRequired("Update your pet's birthday")])
+    submit = SubmitField("Submit Changes")
 
 if __name__ == "__main__":
     app.debug = True
